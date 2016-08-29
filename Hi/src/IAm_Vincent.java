d1fe82ff31a083fb04f78ca36602309c3ad0b0d9
vincentlovemush <vincentyangyang@gmail.com>
2016-08-29 09:43:33 -0700

    This is what I did on 8/29/2016
    A Pikachu

---
+++ Hi/src/IAm_Vincent.java
@@ -0,0 +1,13 @@
+
+public class IAm_Vincent {
+
+	public static void main(String[] args) {
+		System.out.println("I am Vincent!!!HIIIII");
+		System.out.println("I'm bored");
+		System.out.println("\tPikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t  (\\__/)");
+		System.out.println("\t\t  (o^.^)");
+		System.out.println("\t\tz(_(\")(\")");
+	}
+
+}
