f764a31d9ae542ffce659487fcd87af2117e3f4f
vincentlovemush <vincentyangyang@gmail.com>
2016-10-12 10:35:15 -0700

    another another change
    haha

--- TempConvert/src/FarhenheitToCelcius.java
+++ TempConvert/src/FarhenheitToCelcius.java
@@ -1,4 +1,10 @@
 
 public class FarhenheitToCelcius {
-
+	public static void main(String args[]){
+		double f,c;
+		f=50;
+		c=(5*(f-32)/9);
+		
+		System.out.println(f+" Farhenheit is about "+c+" celcius");
+	}
 }
