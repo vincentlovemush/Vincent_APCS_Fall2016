3adaed6b0a3b7d52aaa701a17771d26cbe4b0023
vincentlovemush <vincentyangyang@gmail.com>
2016-11-30 10:44:47 -0800

    checkpoint 3 done
    yayyyyy

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,10 +1,9 @@
 package fracCalc;
 import java.util.*;
-public class FracCalc {
 
-	public static void main(String[] args) 
-	{
-		Scanner input=new Scanner(System.in);
+public class FracCalc {
+	public static void main (String [] args){
+		Scanner input=new Scanner (System.in);
 		System.out.println("Please input");
 		boolean stop=false;
 		while (!stop){
@@ -15,62 +14,78 @@ public class FracCalc {
 			else
 				System.out.println(produceAnswer(question));
 		}
-
-		// TODO: Read the input from the user and call produceAnswer with an equation
-
 	}
-
-	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-	// This function takes a String 'input' and produces the result
-	//
-	// input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-	//      e.g. input ==> "1/2 + 3/4"
-	//        
-	// The function should return the result of the fraction after it has been calculated
-	//      e.g. return ==> "1_1/4"
-	public static String produceAnswer(String input)
-	{ 
+	public static String produceAnswer (String input){
+		String [] question=parseInput(input);
+		int [] firstOperand=parseOperand(question[0]);
+		int [] secondOperand=parseOperand(question[2]);//index 1 is the operator
+		if (question[1].equals("+"))
+			return addFrac(firstOperand,secondOperand);
+		else if (question[1].equals("-"))
+			return subtractFrac(firstOperand,secondOperand);
+		else if (question[1].equals("*"))
+			return multiplyFrac(firstOperand,secondOperand);
+		else
+			return divideFrac(firstOperand,secondOperand);
+	}
+	public static String [] parseInput(String input){
+		String [] answer=input.split(" "); 
+		return answer;
 		
-/*		String answer="";
-		String temp="";
-		if(input.indexOf("+")!=-1){
-			answer=input.substring(input.indexOf("+")+1);
-		}
-		else if(input.indexOf("-")!=-1){
-			answer=input.substring(input.indexOf("-")+1);
-		}
-		else if(input.indexOf("*")!=-1){
-			answer=input.substring(input.indexOf("*")+1);
-		}
-		else{
-			temp=input.substring(input.indexOf("/")+1);
-			answer=temp.substring(temp.indexOf("/")+1);
-		}*/
-		String answer=input.substring(input.lastIndexOf(' ')+1);
-		
-		String finalAnswer="";
+	}
+	public static int []parseOperand (String operand){
+		//also toImproper
+		int [] easyOperand=new int [2];
 		String whole="0";
 		String numerator="0";
 		String denominator="1";
-		if (answer.indexOf('/')==-1){
-			whole=answer;
+		if (operand.indexOf('/')==-1){
+			whole=operand;
 		}
-		else if(answer.indexOf('_')==-1){
-			numerator=answer.substring(0,answer.indexOf('/'));
-			denominator=answer.substring(answer.indexOf('/')+1,answer.length());
+		else if(operand.indexOf('_')==-1){
+			numerator=operand.substring(0,operand.indexOf('/'));
+			denominator=operand.substring(operand.indexOf('/')+1,operand.length());
 		}
 		else{
-			whole=answer.substring(0,answer.indexOf('_'));
-			numerator=answer.substring(answer.indexOf('_')+1,answer.indexOf('/'));
-			denominator=answer.substring(answer.indexOf('/')+1,answer.length());
+			whole=operand.substring(0,operand.indexOf('_'));
+			numerator=operand.substring(operand.indexOf('_')+1,operand.indexOf('/'));
+			denominator=operand.substring(operand.indexOf('/')+1,operand.length());
 		}
-		finalAnswer="whole:"+whole+" numerator:"+numerator+" denominator:"+denominator;
-		return finalAnswer;
-		
-
-		// TODO: Implement this function to produce the solution to the input
+		//changes to improper fraction
+		int wholeNum=Integer.parseInt(whole);
+		int numeratorNum=Integer.parseInt(numerator);
+		easyOperand[1]=Integer.parseInt(denominator);
+		if(wholeNum>=0)
+			easyOperand[0]=wholeNum*easyOperand[1]+numeratorNum;
+		else
+			easyOperand[0]=wholeNum*easyOperand[1]-numeratorNum;
+		return easyOperand;
+		}
+	public static String addFrac (int [] operand1, int [] operand2){
+		String answer="";
+		int commonDen=operand1[1]*operand2[1];
+		int sumOfNumerator=operand1[0]*operand2[1]+operand2[0]*operand1[1];
+		answer=sumOfNumerator+"/"+commonDen;
+		return answer;
 	}
-
-	// TODO: Fill in the space below with any helper methods that you think you will need
-
+	public static String subtractFrac (int [] operand1, int [] operand2){
+		String answer="";
+		operand2[0]=-operand2[0]; //make the second operand's numerator negative
+		return addFrac(operand1, operand2);
+	}
+	public static String multiplyFrac (int [] operand1, int [] operand2){
+		String answer="";
+		int numerator=operand1[0]*operand2[0];
+		int denominator=operand1[1]*operand2[1];
+		answer=numerator+"/"+denominator;
+		return answer;
+	}
+	public static String divideFrac  (int [] operand1, int [] operand2){
+		String answer="";
+		int numerator=operand1[0]*operand2[1];//flips the second operator's numerator and denominator
+		int denominator=operand1[1]*operand2[0];//flips the second operator's numerator and denominator
+		answer=numerator+"/"+denominator;
+		return answer;
+	}
+	
 }
